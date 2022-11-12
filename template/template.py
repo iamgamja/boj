4afdc4ae8c8bd13f46de8771fc5ad7a1f285d32c
iamgamja <hmmthinking@naver.com>
2022-11-12 14:43:27 +0900

    feat: edit template

--- template/template.py
+++ template/template.py
@@ -1,3 +1,8 @@
 import sys
+from decimal import Decimal as D
 from collections import deque
 def input(): return sys.stdin.readline().rstrip()
+
+
+def ins(): return map(int, input().split())
+def ii(): return int(input())
