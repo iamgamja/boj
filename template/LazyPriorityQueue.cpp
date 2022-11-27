03baf8fb88bef5126c77a8015271e2de8b06bbb7
iamgamja <hmmthinking@naver.com>
2022-11-27 13:42:29 +0900

    feat: add cpp template
    * LazyPriorityQueue.cpp

---
+++ template/LazyPriorityQueue.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <cassert>
+
+template <typename T, typename cmp = less<T>>
+class LazyPriorityQueue
+{
+private:
+  priority_queue<T, vector<T>, cmp> q;
+  map<T, int> lazy;
+  map<T, int> count;
+
+public:
+  LazyPriorityQueue(vector<T> arr = {})
+  {
+    for (T x : arr)
+    {
+      q.push(x);
+    }
+  };
+
+  void push(T x)
+  {
+    q.push(x);
+    count[x]++;
+  }
+
+  T pop()
+  {
+    while (!q.empty())
+    {
+      T tmp = q.top();
+      q.pop();
+      if (lazy[tmp] == 0)
+      {
+        count[tmp]--;
+        return tmp;
+      }
+      lazy[tmp]--;
+    }
+    throw runtime_error("Queue is empty.");
+  }
+
+  bool canRemove(T x)
+  {
+    return count[x] != 0;
+  }
+
+  void remove(T x)
+  {
+    assert(canRemove(x));
+
+    lazy[x]++;
+    count[x]--;
+  }
+};
