de07b3c1e1156a53c23445d09157a429406b98e7
iamgamja <hmmthinking@naver.com>
2022-12-04 12:59:42 +0900

    feat: add short template.py

---
+++ template/tp.py
@@ -0,0 +1,1 @@
+import sys;from decimal import Decimal as D;from collections import deque,defaultdict;input=lambda:sys.stdin.readline().rstrip();mi=lambda:map(int,input().split());ii=lambda:int(input())
