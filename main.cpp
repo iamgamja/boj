2e6e02f3b5d231738510ab2a1a4081e53f489f45
iamgamja <hmmthinking@naver.com>
2022-12-11 16:24:48 +0900

    style: cpp template

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void fastio()
-{
+void fastio() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 }
 
-int main(void)
-{
+int main(void) {
 #ifdef ONLINE_JUDGE
   fastio();
 #endif
