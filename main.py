7da878f97c7d79121c98bd4d8399cfaa57cb1011
iamgamja <hmmthinking@naver.com>
2022-12-18 12:22:25 +0900

    edit python template

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ import sys
 from decimal import Decimal as D
 from collections import deque, defaultdict
 def input(): return sys.stdin.readline().rstrip()
-
-
 def mi(): return map(int, input().split())
 def ii(): return int(input())
