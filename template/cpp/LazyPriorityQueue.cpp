a077b358bf0edbcd14ca5de2e9a7c4d145002de5
iamgamja <hmmthinking@naver.com>
2023-02-25 14:41:09 +0900

    .

--- template/cpp/LazyPriorityQueue.cpp
+++ template/cpp/LazyPriorityQueue.cpp
@@ -1,42 +1,33 @@
-#include <iostream>
-using namespace std;
-
-#include <queue>
-#include <map>
 #include <algorithm>
 #include <cassert>
+#include <map>
+#include <queue>
+using namespace std;
 
 template <typename T, typename cmp = less<T>>
-class LazyPriorityQueue
-{
-private:
+class LazyPriorityQueue {
+  private:
   priority_queue<T, vector<T>, cmp> q;
   map<T, int> lazy;
   map<T, int> count;
 
-public:
-  LazyPriorityQueue(vector<T> arr = {})
-  {
-    for (T x : arr)
-    {
+  public:
+  LazyPriorityQueue(vector<T> arr = {}) {
+    for (T x : arr) {
       q.push(x);
     }
   };
 
-  void push(T x)
-  {
+  void push(T x) {
     q.push(x);
     count[x]++;
   }
 
-  T pop()
-  {
-    while (!q.empty())
-    {
+  T pop() {
+    while (!q.empty()) {
       T tmp = q.top();
       q.pop();
-      if (lazy[tmp] == 0)
-      {
+      if (lazy[tmp] == 0) {
         count[tmp]--;
         return tmp;
       }
@@ -45,13 +36,11 @@ public:
     throw runtime_error("Queue is empty.");
   }
 
-  bool canRemove(T x)
-  {
+  bool canRemove(T x) {
     return count[x] != 0;
   }
 
-  void remove(T x)
-  {
+  void remove(T x) {
     assert(canRemove(x));
 
     lazy[x]++;
