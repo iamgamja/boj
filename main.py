f59357a9f7b4de64903e9678321c2d85872011cf
iamgamja <hmmthinking@naver.com>
2023-07-28 15:17:31 +0900

    edit template

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-import sys
-from decimal import Decimal as D
-from collections import deque, defaultdict
-def input(): return sys.stdin.readline().rstrip()
-def mi(): return map(int, input().split())
-def ii(): return int(input())
+import sys;from decimal import Decimal;from collections import deque,defaultdict;input=lambda:sys.stdin.readline().rstrip();mi=lambda f=int:map(f,input().split());ii=lambda:int(input())
+
