e407724535e238f58cbbc02f81e64188ced36fb7
iamgamja <hmmthinking@naver.com>
2024-06-06 15:04:06 +0900

    Update tp.py

--- template/py/tp.py
+++ template/py/tp.py
@@ -1,1 +1,1 @@
-import sys;from decimal import Decimal;from collections import deque,defaultdict;input=lambda:(tmp:=sys.stdin.readline())[~tmp.endswith('\n')::-1][::-1];mi=lambda f=int:map(f,input().split());ii=lambda:int(input())
+import sys;from decimal import Decimal;from collections import deque,defaultdict;input=lambda:_[:-1]if (_:=sys.stdin.readline()).endswith('\n')else _;mi=lambda f=int:map(f,input().split());ii=lambda:int(input());iii=lambda n:[ii()for _ in range(n)]
