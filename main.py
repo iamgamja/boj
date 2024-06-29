27c312c7f769a15adb0e5f2acebdddd0b4d1e2ad
iamgamja <hmmthinking@naver.com>
2024-06-29 21:41:33 +0900

    add gaboja

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 import sys;from decimal import Decimal;from collections import deque,defaultdict;input=lambda:_[:-1]if (_:=sys.stdin.readline()).endswith('\n')else _;mi=lambda f=int:map(f,input().split());ii=lambda:int(input());iii=lambda n:[ii()for _ in range(n)]
+
