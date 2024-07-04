f318d1c5cf2741708baf976a266ca2355f4eef02
iamgamja <hmmthinking@naver.com>
2024-07-04 02:55:16 +0000

    Update ccw.cpp

--- template/cpp/geometry/ccw.cpp
+++ template/cpp/geometry/ccw.cpp
@@ -1,3 +1,12 @@
+template <typename T>
+int sign(T val) {
+  return (T(0) < val) - (val < T(0));
+}
+
 i64 ccw(const Point& a, const Point& b, const Point& c) {
   return (b-a)/(c-a);
+}
+
+i32 sccw(const Point& a, const Point& b, const Point& c) {
+  return sign(ccw(a,b,c));
 }
