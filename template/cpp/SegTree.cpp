0599a9e569fe137e209e90ee2c9613dedea915ad
iamgamja <hmmthinking@naver.com>
2024-07-08 08:03:41 +0900

    Update SegTree.cpp

--- template/cpp/SegTree.cpp
+++ template/cpp/SegTree.cpp
@@ -5,7 +5,7 @@ class SegTree {
   using OP = function<T(T,T)>;
 
   private:
-  i32 n;
+  int n;
   V tree;
   OP op;
   T defaultvalue;
@@ -14,16 +14,16 @@ class SegTree {
   SegTree(const V &a, const OP op, const T defaultvalue): op(op), defaultvalue(defaultvalue) {
     n = a.size();
     tree = vector<T>(2*n);
-    for (i32 i=0; i<n; i++) tree[i+n] = a[i];
-    for (i32 i=n-1; i>0; i--) tree[i] = op(tree[i<<1], tree[i<<1|1]);
+    for (int i=0; i<n; i++) tree[i+n] = a[i];
+    for (int i=n-1; i>0; i--) tree[i] = op(tree[i<<1], tree[i<<1|1]);
   }
 
-  void update(i32 i, T v) {
+  void update(int i, T v) {
     for (tree[i+=n]=v; i>1; i>>=1)
       tree[i>>1] = op(tree[i], tree[i^1]);
   }
 
-  T query(i32 l, i32 r) {
+  T query(int l, int r) {
     T res = defaultvalue;
     for (l+=n,r+=n+1; l<r; l>>=1,r>>=1) {
       if (l&1) res = op(res, tree[l++]);
