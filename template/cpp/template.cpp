eb160e7057606c72d2401e6b8aea84e84950fefc
iamgamja <hmmthinking@naver.com>
2024-07-09 09:40:01 +0900

    Update template.cpp

--- template/cpp/template.cpp
+++ template/cpp/template.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-// using int = int;
+// using int = int; // index 등에서 사용
 using i32 = int;
 using i64 = long long;
 using i128 = __int128;
+// using f32 = float; // DO NOT USE
+using f64 = double;
+using f128 = long double;
 
 int main() {
   cin.tie(0)->sync_with_stdio(0);
