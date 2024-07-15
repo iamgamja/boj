7dd368c9fc5083e8bdfc1fcd1d1fe13ec39b4f52
iamgamja <hmmthinking@naver.com>
2024-07-15 16:22:51 +0900

    Update FenwickTree2d.cpp

--- template/cpp/data_structure/FenwickTree2d.cpp
+++ template/cpp/data_structure/FenwickTree2d.cpp
@@ -9,6 +9,15 @@ class FenwickTree2d {
   int Y;
   int X;
 
+  A prefixQuery(int y_, int x_) {
+    y_++; x_++; // 1-index
+    A res = A();
+    for (int y=y_; y; y-=y&-y)
+      for (int x=x_; x; x-=x&-x)
+        res += tree[y][x];
+    return res;
+  }
+
   public:
   FenwickTree2d(const VVA &a) {
     Y = a.size();
@@ -27,16 +36,7 @@ class FenwickTree2d {
         tree[y][x] += diff;
   }
 
-  A query(int y_, int x_) {
-    y_++; x_++; // 1-index
-    A res = A();
-    for (int y=y_; y; y-=y&-y)
-      for (int x=x_; x; x-=x&-x)
-        res += tree[y][x];
-    return res;
-  }
-
   A query(int y1, int x1, int y2, int x2) {
-    return query(y2,x2) - query(y2,x1-1) - query(y1-1,x2) + query(y1-1,x1-1);
+    return prefixQuery(y2,x2) - prefixQuery(y2,x1-1) - prefixQuery(y1-1,x2) + prefixQuery(y1-1,x1-1);
   }
 };
