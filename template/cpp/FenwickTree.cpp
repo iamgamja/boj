6d20026187974bf42f479a6307643c0129c54498
iamgamja <hmmthinking@naver.com>
2024-07-15 00:40:46 +0000

    Update FenwickTree.cpp

--- template/cpp/FenwickTree.cpp
+++ template/cpp/FenwickTree.cpp
@@ -1,18 +1,14 @@
 template<typename A>
-/** 0-index */
+/** sum, 0-index */
 class FenwickTree {
   using VA = vector<A>;
-  using FM = function<A(A,A)>;
-  using FnM = function<A(A,A)>;
 
   private:
   VA tree;
   int n;
-  FM M;
-  FnM nM;
 
   public:
-  FenwickTree(const VA &a, const FM M, const FnM nM): M(M), nM(nM) {
+  FenwickTree(const VA &a) {
     n = a.size();
     tree = VA(n+1); // 1-index
     for (int i=0; i<n; i++)
@@ -20,10 +16,10 @@ class FenwickTree {
   }
 
   void update(int i, A v) {
-    A diff = nM(v, query(i,i));
+    A diff = v - query(i,i);
     i++; // 1-index
     while (i<=n) {
-      tree[i] = M(tree[i], diff);
+      tree[i] += diff;
       i += i&-i;
     }
   }
@@ -33,7 +29,7 @@ class FenwickTree {
     i++; // 1-index
     A res = A();
     while (i) {
-      res = M(res, tree[i]);
+      res += tree[i];
       i -= i&-i;
     }
     return res;
@@ -41,6 +37,6 @@ class FenwickTree {
 
   /** [l, r] */
   A query(int l, int r) {
-    return nM(query(r), query(l-1));
+    return query(r) - query(l-1);
   }
 };
