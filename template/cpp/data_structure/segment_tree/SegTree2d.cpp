fbfb86354d9f1e880337847e8682694566267736
iamgamja <hmmthinking@naver.com>
2024-07-16 12:42:37 +0900

    Update SegTree2d.cpp

--- template/cpp/data_structure/segment_tree/SegTree2d.cpp
+++ template/cpp/data_structure/segment_tree/SegTree2d.cpp
@@ -14,9 +14,9 @@ class SegTree2d {
     x1 += X; x2 += X;
     A resL = A(), resR = A();
 
-    for (int xL = x1, xR = x2; xL <= xR; xL >>= 1, xR >>= 1) {
-      if (xL & 1) resL = M(resL, tree[y][xL++]);
-      if (~xR & 1) resR = M(tree[y][xR--], resR);
+    for (int L=x1, R=x2; L<=R; L>>=1, R>>=1) {
+      if (L&1) resL = M(resL, tree[y][L++]);
+      if (~R&1) resR = M(tree[y][R--], resR);
     }
     return M(resL, resR);
   }
@@ -41,8 +41,8 @@ class SegTree2d {
     tree[y_+=Y][x_+=X]=v;
 
     // 먼저 같은 y update
-    for (int x=x_; x>1; x>>=1)
-      tree[y_][x>>1] = M(tree[y_][x>>1<<1], tree[y_][x>>1<<1|1]);
+    for (int x=x_>>1; x; x>>=1)
+      tree[y_][x] = M(tree[y_][x<<1], tree[y_][x<<1|1]);
 
     // 이제 y 올라가면서 update
     for (int y=y_>>1; y; y>>=1)
@@ -54,9 +54,9 @@ class SegTree2d {
     y1 += Y; y2 += Y;
     A resL = A(), resR = A();
 
-    for (int yL = y1, yR = y2; yL <= yR; yL >>= 1, yR >>= 1) {
-      if (yL & 1) resL = M(resL, queryX(yL++, x1, x2));
-      if (~yR & 1) resR = M(queryX(yR--, x1, x2), resR);
+    for (int L=y1, R=y2; L<=R; L>>=1, R>>=1) {
+      if (L&1) resL = M(resL, queryX(L++, x1, x2));
+      if (~R&1) resR = M(queryX(R--, x1, x2), resR);
     }
     return M(resL, resR);
   }
