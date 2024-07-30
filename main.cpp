2d918fec446812656811b3e1e7da90c41acd3047
iamgamja <hmmthinking@naver.com>
2024-07-30 17:11:52 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-// using int = int; // index 등에서 사용
-using i32 = int;
-using i64 = long long;
+// using int = int32_t; // index 등에서 사용
+using i32 = int32_t;
+using i64 = int64_t;
 using i128 = __int128;
-// using f32 = float; // DO NOT USE
+// using f32 = float; // 사용하지 않음
 using f64 = double;
 using f128 = long double;
+const i32 inf32 = 0x3f3f3f3f;
+const i64 inf64 = 0x3f3f3f3f3f3f3f3fLL;
 
 int main() {
   cin.tie(0)->sync_with_stdio(0);
