9be8cd6bddf37f0b48779f5bdd33119285ffc056
iamgamja <hmmthinking@naver.com>
2024-08-01 13:55:00 +0000

    Update Point.cpp

--- template/cpp/geometry/Point.cpp
+++ template/cpp/geometry/Point.cpp
@@ -3,36 +3,38 @@ class Point {
   i64 x, y;
 
   Point(): x(0), y(0) {}
-  Point(i64 x, i64 y) : x(x), y(y) {}
+  Point(i64 x, i64 y): x(x), y(y) {}
 
-  friend istream& operator>>(istream& is, Point& p) {
+  friend istream& operator>>(istream &is, Point &p) {
     return is >> p.x >> p.y;
   }
 
-  friend ostream& operator<<(ostream& os, const Point& p) {
+  friend ostream& operator<<(ostream &os, const Point &p) {
     return os << "(" << p.x << ", " << p.y << ")";
   }
 
-  Point operator+(const Point& o) const {
+  Point operator+(const Point &o) const {
     return Point(x+o.x, y+o.y);
   }
-  Point operator-(const Point& o) const {
+  Point operator-(const Point &o) const {
     return Point(x-o.x, y-o.y);
   }
   Point operator-() const {
     return Point(-x, -y);
   }
-  i64 operator*(const Point& o) const {
+  /** 내적 */
+  i64 operator*(const Point &o) const {
     return x*o.x + y*o.y;
   }
-  i64 operator/(const Point& o) const {
+  /** 외적 */
+  i64 operator/(const Point &o) const {
     return x*o.y - y*o.x;
   }
 
-  auto operator<=>(const Point& o) const {
+  auto operator<=>(const Point &o) const {
     return (x <=> o.x) != 0 ? (x <=> o.x) : (y <=> o.y);
   }
-  bool operator==(const Point& o) const {
+  bool operator==(const Point &o) const {
     return x == o.x && y == o.y;
   }
 };
