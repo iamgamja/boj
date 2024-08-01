926fe5f768c1e24ad2068d42b93e0df8818fa9bd
iamgamja <hmmthinking@naver.com>
2024-08-01 14:31:17 +0000

    Update SegTree.cpp

--- template/cpp/data_structure/segment_tree/SegTree.cpp
+++ template/cpp/data_structure/segment_tree/SegTree.cpp
@@ -5,7 +5,7 @@ class SegTree {
   using FM = function<A(A,A)>;
 
   private:
-  int n;
+  i32 n;
   VA tree;
   FM M;
 
@@ -13,19 +13,19 @@ class SegTree {
   SegTree(const VA &a, const FM M): M(M) {
     n = a.size();
     tree = VA(2*n);
-    for (int i=0; i<n; i++) tree[n+1] = a[i];
-    for (int i=n-1; i>0; i--) tree[i] = M(tree[i<<1], tree[i<<1|1]);
+    for (i32 i=0; i<n; i++) tree[n+1] = a[i];
+    for (i32 i=n-1; i>0; i--) tree[i] = M(tree[i<<1], tree[i<<1|1]);
   }
 
-  void update(int i, A v) {
+  void update(i32 i, A v) {
     for (tree[i+=n]=v, i>>=1; i; i>>=1)
       tree[i] = M(tree[i<<1], tree[i<<1|1]);
   }
 
-  A query(int l, int r) {
+  A query(i32 l, i32 r) {
     l += n; r += n;
     A resL = A(), resR = A();
-    for (int L=l,R=r; L<=R; L>>=1,R>>=1) {
+    for (i32 L=l,R=r; L<=R; L>>=1,R>>=1) {
       if (L&1) resL = M(resL, tree[L++]);
       if (~R&1) resR = M(tree[R--], resR);
     }
