19702894a477a2cbf0d886d46b0664efbf71131e
iamgamja <hmmthinking@naver.com>
2024-08-01 14:02:55 +0000

    Update UnionFind.cpp

--- template/cpp/data_structure/UnionFind.cpp
+++ template/cpp/data_structure/UnionFind.cpp
@@ -1,45 +1,33 @@
 class UnionFind {
   private:
-  const int none = INT_MIN;
-  vector<int> nodes;
-  vector<int> parent;
-  vector<int> depth;
+  vector<i32> p;
+  vector<i32> d;
 
   public:
-  // [s, e]
-  UnionFind(int s, int e) {
-    for (int i = 0; i < s; i++) { // index를 맞추기 위한
-      nodes.push_back(none);
-      parent.push_back(none);
-      depth.push_back(none);
-    }
-
-    for (int i = s; i <= e; i++) {
-      nodes.push_back(i);
-      parent.push_back(none);
-      depth.push_back(1);
+  // [0, e]
+  UnionFind(i32 e) {
+    p.resize(e+1);
+    d.resize(e+1);
+    for (i32 i = 0; i <= e; i++) {
+      p[i] = i;
+      d[i] = 1;
     }
   }
 
-  int find(int target) {
-    if (parent[target] == none)
-      return target;
-    return parent[target] = find(parent[target]);
+  i32 find(i32 x) {
+    if (p[x] == x) return x;
+    return p[x] = find(p[x]);
   }
 
   // O(logN)
-  void merge(int a, int b) {
-    int ar = find(a);
-    int br = find(b);
-
+  void merge(i32 a, i32 b) {
+    i32 ar = find(a);
+    i32 br = find(b);
     if (ar == br) return;
 
-    if (depth[ar] > depth[br])
-      parent[br] = ar; // a에 b 합치기
-    else
-      parent[ar] = br; // b에 a 합치기
-
-    if (depth[ar] == depth[br])
-      depth[br]++; // 만약 같았다면 b에 a를 합쳤을 것이고, 깊이가 1 증가할 것
+    if (d[ar] < d[br]) swap(ar, br);
+    
+    p[br] = ar; // a에 b 합치기
+    if (d[ar] == d[br]) d[ar]++;
   }
 };
