1cc4d943681af09c8c27c9ca784477221040d720
iamgamja <hmmthinking@naver.com>
2024-08-01 14:25:42 +0000

    Update LazyPriorityQueue.cpp

--- template/cpp/data_structure/LazyPriorityQueue.cpp
+++ template/cpp/data_structure/LazyPriorityQueue.cpp
@@ -2,8 +2,9 @@ template <typename T, typename cmp = less<T>>
 class LazyPriorityQueue {
   private:
   priority_queue<T, vector<T>, cmp> q;
-  map<T, int> lazy;
-  map<T, int> count;
+  map<T, i32> lazy;
+  map<T, i32> count;
+  i32 countall = 0;
 
   public:
   LazyPriorityQueue(vector<T> &arr = {}) {
@@ -13,22 +14,29 @@ class LazyPriorityQueue {
   void push(T x) {
     q.push(x);
     count[x]++;
+    countall++;
   }
 
   void remove(T x) {
     lazy[x]++;
     count[x]--;
+    countall--;
+  }
+
+  bool empty() {
+    return countall != 0;
   }
 
   T pop() {
     while (!q.empty()) {
-      T tmp = q.top();
-      q.pop();
+      T tmp = q.top(); q.pop();
       if (lazy[tmp] == 0) {
         count[tmp]--;
+        countall--;
         return tmp;
       }
       lazy[tmp]--;
+      countall--;
     }
     throw runtime_error("Queue is empty.");
   }
