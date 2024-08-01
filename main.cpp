f1995bf594fbe07fe8487ffc25607a9b5a0008c4
iamgamja <hmmthinking@naver.com>
2024-08-01 22:42:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-// using int = int32_t; // index 등에서 사용
 using i32 = int32_t;
 using i64 = int64_t;
 using i128 = __int128;
-// using f32 = float; // 사용하지 않음
+#define int
 using f64 = double;
 using f128 = long double;
 const i32 inf32 = 0x3f3f3f3f;
@@ -15,7 +14,7 @@ istream& operator>>(istream &is, pii &x) {
 }
 #define ALL(v) (v).begin(), (v).end()
 
-int main() {
+i32 main() {
   cin.tie(0)->sync_with_stdio(0);
 
   // code
