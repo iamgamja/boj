3dfba704335cd78693ebe4c77c262c0e0f49852d
김주원 <hmmthinking@naver.com>
2024-08-22 08:27:46 +0900

    Update template.cpp

--- template/cpp/template.cpp
+++ template/cpp/template.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 using i32 = int32_t;
+#define int DELETED
 using i64 = int64_t;
 using i128 = __int128;
-#define int DELETED
 using f64 = double;
 using f128 = long double;
 const i32 inf = 0x3f3f3f3f;
 const i64 inf64 = 0x3f3f3f3f3f3f3f3fLL;
 #define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) { for (auto &x: arr) is >> x; return is; }
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> &arr) { for (auto &x: arr) os << x << " "; return os; }
 
 i32 main() {
   cin.tie(0)->sync_with_stdio(0);
