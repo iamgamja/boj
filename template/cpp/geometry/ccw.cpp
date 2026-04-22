4aa07a532759e301c8de424f11074e655d7df91d
김주원 <hmmthinking@naver.com>
2026-04-22 02:01:06 +0000

    add sign

--- template/cpp/geometry/ccw.cpp
+++ template/cpp/geometry/ccw.cpp
@@ -2,6 +2,11 @@ i64 ccw(const Point &a, const Point &b, const Point &c) {
   return (b-a)/(c-a);
 }
 
+template <typename T>
+i32 sign(T v) {
+  return (T(0) < v) - (v < T(0));
+}
+
 i32 sccw(const Point &a, const Point &b, const Point &c) {
   return sign(ccw(a,b,c));
 }
