a641971b697442a37750ccaeb06c03d574586aa4
김주원 <hmmthinking@naver.com>
2026-06-05 22:09:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from collections import deque,defaultdict
 import os
-_nt=iter((os.read(0,os.fstat(0).st_size)if os.fstat(0).st_size else open(0).read()).split()).__next__
+_nt=iter(os.read(0,os.fstat(0).st_size).split()).__next__
 tok=lambda:_nt().decode()
 def ii(*A):return[ii(*A[1:])for _ in range(A[0])]if A else int(tok())
+
+###
+
