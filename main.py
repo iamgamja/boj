13aa4dad6d88de3312ceb518813b1628556c50a9
김주원 <hmmthinking@naver.com>
2026-06-05 17:31:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from collections import deque,defaultdict
 import os
-_nt=iter(os.read(0,os.fstat(0).st_size).split()).__next__
+_nt=iter((os.read(0,os.fstat(0).st_size)if os.fstat(0).st_size else open(0).read()).split()).__next__
 tok=lambda:_nt().decode()
 def ii(N=-1):return int(tok())if N<0 else[ii()for _ in range(N)]
 
-def main():
-  pass
-
-main()
