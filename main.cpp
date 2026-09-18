cba9c9a41175fe553173d465072adb3a74cf094b
김주원 <hmmthinking@naver.com>
2026-09-18 18:13:35 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ using i128 = __int128;
 using VI = vector<i32>;
 using VVI = vector<VI>;
 
+template <typename T> istream& operator>>(istream& in, vector<T>& v) { for (auto& x : v) in >> x; return in; }
+template <typename T> ostream& operator<<(ostream& out, const vector<T>& v) { for (i32 i=0; i < v.size(); i++) { if (i) out << ' '; out << v[i]; } return out; }
+
 i32 main() {
   cin.tie(0)->sync_with_stdio(0);
 
