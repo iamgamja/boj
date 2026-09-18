eca4a875d0af17f2bd3c597ba9e7e947df6fdd97
김주원 <hmmthinking@naver.com>
2026-09-18 18:13:07 +0900

    Add stream operators for vector input/output

--- template/cpp/default.hpp
+++ template/cpp/default.hpp
@@ -7,3 +7,6 @@ using i128 = __int128;
 
 using VI = vector<i32>;
 using VVI = vector<VI>;
+
+template <typename T> istream& operator>>(istream& in, vector<T>& v) { for (auto& x : v) in >> x; return in; }
+template <typename T> ostream& operator<<(ostream& out, const vector<T>& v) { for (i32 i=0; i < v.size(); i++) { if (i) out << ' '; out << v[i]; } return out; }
